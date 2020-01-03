7a5a2b508f733b5935bf044eb430925ea1d443b8
Rich Moyse <rich@moyse.us>
2020-01-03 01:25:40 -0500

    prevent infinite recursion when command is implemented by recurcmd

--- component/recurcmd.source.sh
+++ component/recurcmd.source.sh
@@ -26,11 +26,12 @@ recurcmd_report (){
 }
 
 recurcmd_run(){
-	local cmmdNAME="$1"
+	local -r cmmdNAME="$1"
+	local -r cmmdIS_RECURCMD="$2"
 	local pipeSTATE
 	recurcmd__pipe_option_save pipeSTATE
 	set -o pipefail
-	recurcmd_list_gen "$cmmdNAME" | recurcmd__run
+	recurcmd_list_gen "$cmmdNAME" | recurcmd__run "$cmmdIS_RECURCMD"
 	recurcmd__pipe_option_restore pipeSTATE
 }
 
@@ -43,16 +44,26 @@ recurcmd__pipe_option_restore(){
 }
 
 recurcmd__run(){
+	local cmmdIS_RECURCMD="$1"
+	if [ "$cmmdIS_RECURCMD" != 'true' ]; then
+		cmmdIS_RECURCMD='false'
+	fi
+	local -r cmmdIS_RECURCMD
+
 	local cmmdDIR
 	local cmmdFILE_PATH
 	local cmmdNAME
 	while read -r cmmdFILE_PATH; do
 		cmmdDIR="$(dirname "$cmmdFILE_PATH")"
 		cmmdNAME="$(basename "$cmmdFILE_PATH")"
+		if $cmmdIS_RECURCMD && [ "$cmmdDIR" = '.' ]; then
+			# avoid infinite recursion
+			continue
+		fi
 		recurcmd_report "$cmmdNAME" "$cmmdDIR"
 		(
 			cd "$cmmdDIR"
-			./$cmmdNAME
+			./"$cmmdNAME"
 		)
 	done
 }
